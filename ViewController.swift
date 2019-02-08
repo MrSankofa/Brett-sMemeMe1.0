5013b640326c1c7c8f503146a098742d93ebb869
Brett Cunningham <bwcunninghamii@gmail.com>
2019-02-08 09:09:48 -0500

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Brett's MeMe 1.0
+//
+//  Created by Brett Cunningham on 2/8/19.
+//  Copyright © 2019 Altruistic Software Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
